1f449c76b3dfe01f832447a772e87bd9b9ca09c4
Moks8 <cristinagutierrezrub@gmail.com>
2020-06-10 00:36:25 +0200

    Quit/Space

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame as pg
 from pygame.locals import *
-import sys
+import sys,random
+ 
+
 
 BACKGROUND = (50,50,50)
 YELLOW = (255,255,0)
+WHITE = (255,255,255)
+WIN_GAME_SCORE = 3
 
 class Ball: 
     def __init__(self):
@@ -16,6 +20,10 @@ class Ball:
 
         self.image = pg.Surface((self.h, self.w))
         self.image.fill(YELLOW)
+        self.ping= pg.mixer.Sound("./resources/sounds/ping.wav")
+        self.lost_point = pg.mixer.Sound("./resources/sounds/lost-point.wav")
+
+        #buscar sonindos en freesound.org
 
     @property
     def posx(self):
@@ -27,19 +35,42 @@ class Ball:
 
     def move(self, limSupX, limSupY):
         if self.Cx >= limSupX or self.Cx <=0:
-            self.vx *= -1
+            self.vx = 0
+            self.vy = 0
+            self.lost_point.play()
 
         if self.Cy >= limSupY or self.Cy <=0:
             self.vy *= -1
+            self.ping.play()
+        #choca con la pared
                 
         self.Cx += self.vx
         self.Cy += self.vy
+    
+    def comprobarChoque (self,something): #desde la bola voy a comprobar que choco con algo, y si choco, modifico mi velovidad
+        dx = abs(self.Cx - something.Cx)#valor absoluto
+        dy = abs(self.Cy - something.Cy)
+
+        if dx < (self.w + something.w) //2 and dy < (self.h+ something.h) //2:
+            self.vx*= -1
+            self.Cx += self.vx
+            self.Cy += self.vy #forzamos un paso para que no rebote hacia el otro lado(me coloca la bola un paso mas adelante sin pintarla, asi no se me va hacia la pared)
+            self.ping.play()
+        
+        #comprobar si hay choque:
+        #si hay choque, cambiar el signo de mi velocidad x (vx)
+
+    def reset(self):
+        self.vx = random.choice([-7,-5,5,7])
+        self.vy = random.choice([-7,-5,5,7])
+        self.Cx = 400
+        self.Cy = 300
 
 class Racket:
-    def __init__(self):
+    def __init__(self,Cx):
         self.vx = 0
         self.vy = 5
-        self.Cx = 30
+        self.Cx = Cx
         self.Cy = 300
         self.h = 100
         self.w = 25
@@ -73,41 +104,129 @@ class Game:
         self.fondo = pg.image.load("resources/fondo.jpg")
         self.pantalla.blit(self.fondo,(0,0))
         self.ball = Ball()
-        self.playerOne= Racket()
+        self.playerOne= Racket(30)
+        self.playerTwo = Racket(770) #al instanciar, que no se nos olvide pintar
 
+        self.status = "Partida"
+
+        self.font = pg.font.Font("./resources/fonts/font.ttf",40)
+        self.fontGrande = pg.font.Font("./resources/fonts/font.ttf",60)
+
+        self.marcadorOne= self.font.render("0",True,WHITE)
+        self.marcadorTwo= self.font.render("0",True,WHITE)
+
+        self.text_game_over = self.fontGrande.render("GAME OVER",True,YELLOW)
+        self.text_insert_coin = self.font.render("<SPACE> - Inicio partida",True,WHITE)
+
+        self.scoreOne = 0
+        self.scoreTwo = 0
 
         pg.display.set_caption("Pong")
 
-    def main_loop(self):
+    def bucle_partida(self):
         game_over = False
+        self.scoreOne = 0
+        self.scoreTwo= 0 
+        self.marcadorOne = self.font.render(str(self.scoreOne),True,WHITE)
+        self.marcadorTwo = self.font.render(str(self.scoreTwo),True,WHITE)
 
         while not game_over:
-            for event in pg.event.get():
-                if event.type == QUIT:
-                    game_over = True
-
-                if event.type == KEYDOWN:    #se hunde, KEYUP, se libera
-                    if event.key == K_UP:
-                        self.playerOne.vy = -5 #la velocidad refleja el estado de movimiento
-                    if event.key == K_DOWN:
-                        self.playerOne.vy = 5
-
-            key_pressed = pg.key.get_pressed() #lista donde te deja almacenada las teclas que han sido pulsadas
-            if key_pressed[K_UP]:
-                self.playerOne.vy = -5
-            elif key_pressed[K_DOWN]:
-                self.playerOne.vy = 5
-            else:
-                self.playerOne.vy = 0 # la velocidad solo esta incluida cuando solo lo estoy pulsando
-            self.pantalla.blit(self.fondo,(0,0))
-            self.pantalla.blit(self.ball.image,(self.ball.posx,self.ball.posy))
-            self.pantalla.blit(self.playerOne.image,(self.playerOne.posx,self.playerOne.posy))
+            game_over = self.handlenEvent()
 
             self.ball.move(800, 600)
             self.playerOne.move(800,600)
+            self.playerTwo.move(800,600)
+            #comprobar si bola toca player 1 y player 2 y cambiar su velocidad si toca
+            self.ball.comprobarChoque(self.playerOne)
+            self.ball.comprobarChoque(self.playerTwo)
+
+            if self.ball.vx == 0 and self.ball.vy == 0 :
+                if self.ball.Cx >=800:
+                    self.scoreOne += 1
+                    self.marcadorOne = self.font.render(str(self.scoreOne),True,WHITE)
+                if self.ball.Cx <= 0:
+                    self.scoreTwo += 1
+                    self.marcadorTwo = self.font.render(str(self.scoreTwo),True,WHITE)
+
+                if self.scoreOne == WIN_GAME_SCORE or self.scoreTwo == WIN_GAME_SCORE:
+                    game_over = True
+
+                self.ball.reset()
+
+
+            self.pantalla.blit(self.fondo,(0,0))
+            self.pantalla.blit(self.ball.image,(self.ball.posx,self.ball.posy))
+            self.pantalla.blit(self.playerOne.image,(self.playerOne.posx,self.playerOne.posy))
+            self.pantalla.blit(self.playerTwo.image,(self.playerTwo.posx,self.playerTwo.posy))
+            self.pantalla.blit(self.marcadorOne,(30,10))
+            self.pantalla.blit(self.marcadorTwo,(740,10))
+
+            
+
+            pg.display.flip()#actualizar la pantalla
+
+        self.status = "Inicio"
+
+
+    def bucle_inicio(self):
+        inicio_partida = False
+        while not inicio_partida:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                 self.quit()
+                 
+                if event.type == KEYDOWN:    #se hunde, KEYUP, se libera
+                    if event.key == K_SPACE:
+                        inicio_partida = True
+
+            self.pantalla.fill((0,0,255))
+            self.pantalla.blit(self.text_game_over,(100,100))
+            self.pantalla.blit(self.text_insert_coin,(100,200))
 
             pg.display.flip()
+        
+        self.status = "Partida"
 
+    
+
+    def handlenEvent(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                 self.quit()
+            '''
+            if event.type == KEYDOWN:    #se hunde, KEYUP, se libera
+                if event.key == K_UP:
+                    self.playerOne.vy = -5 #la velocidad refleja el estado de movimiento
+                if event.key == K_DOWN:
+                    self.playerOne.vy = 5
+            '''    
+                            
+            key_pressed = pg.key.get_pressed() #lista donde te deja almacenada las teclas que han sido pulsadas
+            if key_pressed[K_UP]:
+                self.playerTwo.vy = -5
+            elif key_pressed[K_DOWN]:
+                self.playerTwo.vy = 5
+            else:
+                self.playerTwo.vy = 0 # la velocidad solo esta incluida cuando solo lo estoy pulsando
+
+            if key_pressed[K_w]:
+                self.playerOne.vy = -5
+            elif key_pressed[K_z]:
+                self.playerOne.vy = 5
+            else:
+                self.playerOne.vy = 0
+
+        return False
+
+    def main_loop(self):
+        
+        while True:
+            if self.status =="Partida":
+                self.bucle_partida()
+            else:
+                self.bucle_inicio()
+
+            
     def quit(self):
         pg.quit()
         sys.exit()
